78158dc9f0f31b8dc7beb01cd3489ae82b02af4f
Andrew0089 <andrewordonez00@gmail.com>
2024-09-25 19:35:54 -0400

    Update direction movement
    Change script so the direction changes randomly

--- Assets/01-Apple Picker/Scripts/AppleTree C# script.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree C# script.cs
@@ -13,7 +13,7 @@ public class AppleTree : MonoBehaviour
     // Distance where AppleTree turns around
     public float leftAndRightEdge = 20f;
     // Chance that the AppleTree will change direction
-    public float chanceToChangeDirection = 0.1f;
+    public float chanceToChangeDirection = 0.02f;
     // Rate at which Apples will be instantiated
     public float secondsBetweenAppleDrop = 1f;
 
@@ -25,11 +25,20 @@ public class AppleTree : MonoBehaviour
     void Update()
     {
         // Basic Movement
-        // Changing Direction
+      
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
-    }
+        // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move
+        }
+        else if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1; // Move 
+        }
+        }
 }
 
          
