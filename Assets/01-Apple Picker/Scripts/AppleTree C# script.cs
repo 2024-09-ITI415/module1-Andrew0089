684c46e8ff2bbc1ae8ecfb7a4dea2fb8403760ac
Andrew0089 <andrewordonez00@gmail.com>
2024-09-25 18:34:11 -0400

    Adding code
    Added speed and fixed some code up to fix some errors within the inititial code so can bbe played

--- Assets/01-Apple Picker/Scripts/AppleTree C# script.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree C# script.cs
@@ -7,20 +7,29 @@ public class AppleTree : MonoBehaviour
 {
     [Header("Set in Inspector")]
     // Prefab for instantiating apples
-    public GameObject applePrefab;
+    public GameObject ApplePrefab;
     // Speed at which the AppleTree moves
-    public float speed = 1f;
+    public float speed = 10f;
     // Distance where AppleTree turns around
     public float leftAndRightEdge = 20f;
-    // Chance that the AppleTree will change di
-    public float chanceToChangeDirection
-// Rate at which Apples will be instantiate
-public float secondsBetweenAppleDrop
-void Start()
+    // Chance that the AppleTree will change direction
+    public float chanceToChangeDirection = 0.1f;
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrop = 1f;
+
+    void Start()
     {
         // Dropping apples every second
     }
+
     void Update()
     {
-// Basic Movement
-// Changing Direction
+        // Basic Movement
+        // Changing Direction
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
+
+         
